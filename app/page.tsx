ff85480de48f9e55a6827e7b3257d5bd55c5b9d0
Vincent <vincent.dagoury@gmail.com>
2023-11-19 19:13:30 +0100

    First simple update on content

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">my portfolio</h1>
+      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">Vincent DAGOURY</h1>
       <p className="prose prose-neutral dark:prose-invert">
-        This is your new portfolio.
+        Welcome to my portfolio website. 
+        I am a CTO, Full Stack developer of 25 years of experience with a passion for web development. 
+        I am also a member of the 
       </p>
     </section>
   );
